1ae18a2bd3673bb70109c1acb08010ff8167ff87
Tomasz Śliwiński <tsliwinski.86@gmail.com>
2023-06-21 19:44:07 +0200

    feat: add API endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://3dxa2uyxq3.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://ke488v1zh0.execute-api.us-east-1.amazonaws.com",
   bff: "https://3dxa2uyxq3.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
